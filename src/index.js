91d2f93d41101519fe4ec17ffb39284f075d445c
Gabriel <gabrielmurryedh@gmail.com>
2022-09-14 14:59:47 -0700

    huge update. added backend. app is now MERN. added mongoDB, node, express, axios. Added log in and sign up page. Added navbar to wordle. updated styling for everything including mobile.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./styles/formStyles.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
