d625f70f7d18ffcdb3826cf9f2b56faf058f07ee
Gabriel <gabrielmurryedh@gmail.com>
2022-09-14 22:26:08 -0700

    quick fix in axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500";
-// "http://localhost:3500"
-// "https://wordle-api-usd8.onrender.com"
+const BASE_URL = "https://wordle-api-usd8.onrender.com";
+// "http://localhost:3500" for testing
+// "https://wordle-api-usd8.onrender.com" for deployment
 
 export default axios.create({
   baseURL: BASE_URL,
