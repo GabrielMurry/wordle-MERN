73ec05049aaefd6dd3a2a9b468e72626f1b27653
Gabriel <gabrielmurryedh@gmail.com>
2022-09-16 12:32:23 -0700

    updated Base_url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const BASE_URL = "https://wordle-api-usd8.onrender.com";
+const BASE_URL = "https://wordle-mern-api.onrender.com";
 // "http://localhost:3500" for testing
-// "https://wordle-api-usd8.onrender.com" for deployment
+// "https://wordle-mern-api.onrender.com" for deployment
 
 export default axios.create({
   baseURL: BASE_URL,
