d01a37845ca8ce0b21ba1d0e6c9cf315ae2272ac
Gabriel <gabrielmurryedh@gmail.com>
2023-02-05 11:21:17 -0800

    new backend base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500";
+const BASE_URL = "https://wordle-mern-backend.adaptable.app/";
 // "http://localhost:3500" for testing
 // "http://192.168.50.29:3500" for testing on network (other than local machine) - ex: mobile
 // "https://wordle-mern-api.onrender.com" for deployment
-// "https://api.render.com/deploy/srv-ccictsha6gdmtlafkbm0?key=8Hd1kY_B9Qg" deploy hook
+// "https://api.render.com/deploy/srv-ccictsha6gdmtlafkbm0?key=8Hd1kY_B9Qg" deploy hook??
+// "https://wordle-mern-backend.adaptable.app/"
 
 export default axios.create({
   baseURL: BASE_URL,
